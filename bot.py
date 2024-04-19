d4ef23f78565fe555c9649d031cc52a4b600e11a
CodingPenguin1 <ryan.j.slater.2@gmail.com>
2024-04-19 19:05:51 -0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,120 @@
+import os
+import discord
+from discord import app_commands
+from dotenv import load_dotenv
+import pandas as pd
+from tabulate import tabulate
+from zipfile import ZipFile
+from replay_parser import parse_replay_directory
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(1120789695148851291))
+
+    # Set status to idle, "awaiting replay file"
+    await client.change_presence(activity=discord.Game(name='awaiting replay file'), status=discord.Status.idle)
+
+
+@tree.command(
+    name='parse',
+    description='Upload a zip file of match replays you wish to parse with this command',
+    guild=discord.Object(1120789695148851291)
+)
+async def parse_replay(interaction, file: discord.Attachment):
+    print('Parse command triggered')
+
+    # Set status to active, "parsing replays"
+    await client.change_presence(activity=discord.Game(name='parsing replays'), status=discord.Status.online)
+
+    # Defer interaction to allow for processing time
+    await interaction.response.defer()
+
+    # Check if file is a zip file
+    if not file.filename.endswith('.zip'):
+        print(f'Invalid file type: {file.filename}')
+        await interaction.response.send_message('Please upload a zip file')
+        return
+
+    # Save the zip file
+    with open(file.filename, 'wb') as f:
+        print(f'Saving {file.filename}')
+        await file.save(os.path.join('input_buffer', file.filename))
+
+    # Delete the file from the root directory
+    os.remove(file.filename)
+
+    # Extract the zip file to cache/
+    with ZipFile(os.path.join('input_buffer', file.filename), 'r') as zip_ref:
+        print(f'Extracting {file.filename} to cache/')
+        zip_ref.extractall('cache')
+
+    # Delete the zip file
+    os.remove(os.path.join('input_buffer', file.filename))
+
+    # Find all directories in cache/ that contain .rec files
+    replay_directories = []
+    for root, dirs, files in os.walk('cache'):
+        if any(file.endswith('.rec') for file in files):
+            replay_directories.append(root)
+    replay_directories.sort()
+
+    print(replay_directories)
+
+    # Parse each replay
+    map_dfs, player_dfs = [], []
+    for replay in replay_directories:
+        print(f'Parsing {replay}')
+        map_df, player_df = parse_replay_directory(replay)
+        map_dfs.append(map_df)
+        player_dfs.append(player_df)
+
+    # Concatenate the dataframes
+    map_df = pd.concat(map_dfs)
+    player_df = pd.concat(player_dfs)
+
+    # Delete the cached replays
+    for replay in replay_directories:
+        print(f'Deleting {replay}')
+        for root, dirs, files in os.walk(replay):
+            for file in files:
+                os.remove(os.path.join(root, file))
+        os.rmdir(replay)
+
+    # Wait for the replays to be parsed before sending the stats
+    print('Waiting for stats to be generated')
+    while len(map_dfs) < len(replay_directories):
+        pass
+    print('Stats generated')
+
+    # Save dataframes to file
+    map_df.to_csv('map_stats.csv', index=False)
+    player_df.to_csv('player_stats.csv', index=False)
+
+    # Send files
+    # await interaction.response.send_message('', files=[discord.File('map_stats.csv'), discord.File('player_stats.csv')])
+    await interaction.followup.send('', files=[discord.File('map_stats.csv'), discord.File('player_stats.csv')])
+
+    # Delete the files
+    os.remove('map_stats.csv')
+    os.remove('player_stats.csv')
+
+    # Set status back to default
+    await client.change_presence(activity=discord.Game(name='awaiting replay file'), status=discord.Status.idle)
+
+
+def main():
+    client.run(TOKEN)
+
+
+if __name__ == '__main__':
+    main()
---
+++ replay_parser.py
@@ -0,0 +1,297 @@
+import subprocess
+import os
+import pandas as pd
+import zipfile
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from time import sleep, time
+from datetime import datetime
+from colorama import Fore
+from fuzzywuzzy import fuzz
+import shutil
+
+INFO = f'{Fore.GREEN}[INF]{Fore.RESET} '
+WARN = f'{Fore.YELLOW}[WRN]{Fore.RESET} '
+ERROR = f'{Fore.RED}[ERR]{Fore.RESET} '
+ACTION = f'{Fore.CYAN}[ACT]{Fore.RESET} '
+
+client = None
+roster_sheet = None
+
+
+def parse_replay_directory(replay_dir):
+    # === Run r6-dissect on extracted replay ===
+    # For folder in match_dir, run r6-dissect
+    replay_json = {}
+    print(INFO + f'   Running r6-dissect on {replay_dir}')
+    replay_json = json.loads(subprocess.run(['./r6-dissect', replay_dir], capture_output=True).stdout.decode('utf-8'))
+
+    # Sort rounds by ascending sum of scores
+    replay_json['rounds'] = sorted(replay_json['rounds'], key=lambda x: x['teams'][0]['score'] + x['teams'][1]['score'])
+
+    # === Generate stats dataframes from r6-dissect output ===
+    # Player Stats
+    print(INFO + '   Parsing player stats')
+    player_df = parse_json_player_stats(replay_json)
+    map_df = parse_map_stats(replay_json)
+    return map_df, player_df
+
+def parse_map_stats(replay_json):
+    map_stats_df = pd.DataFrame(columns=[
+        'Map',
+        'Round Number',
+        'Team 1',
+        'Team 2',
+        'Team 1 Score',
+        'Team 2 Score',
+        'ATK Team',
+        'DEF Team',
+        'Site',
+        'Winning Team',
+        'Winning Side',
+        'Won by Objective'
+    ])
+
+    for round_ in replay_json['rounds']:
+        if round_['matchFeedback'] is None:
+            continue
+
+        # Map
+        map_ = round_['map']['name']
+
+        # Round number
+        round_num = round_['roundNumber'] + 1
+
+        # Team names
+        team_names = [round_['teams'][0]['name'], round_['teams'][1]['name']]
+
+        # Scores
+        team_1_score = round_['teams'][0]['score']
+        team_2_score = round_['teams'][1]['score']
+
+        # ATK and DEF teams
+        atk_team = team_names[0] if round_['teams'][0]['role'] == 'Attack' else team_names[1]
+        def_team = team_names[0] if round_['teams'][0]['role'] == 'Defend' else team_names[1]
+
+        # Site played
+        try:
+            site = round_['site']
+        except KeyError:
+            site = 'N/A'
+
+        # Winning team and side
+        if round_['teams'][0]['won']:
+            winning_team = team_names[0]
+            winning_side = 'ATK' if round_['teams'][0]['role'] == 'Attack' else 'DEF'
+        else:
+            winning_team = team_names[1]
+            winning_side = 'ATK' if round_['teams'][1]['role'] == 'Attack' else 'DEF'
+
+        # Won by objective
+        won_by_objective = False
+        for event in round_['matchFeedback']:
+            if event['type']['name'] == 'DefuserPlantComplete' or event['type']['name'] == 'DefuserDisableComplete':
+                won_by_objective = True
+
+        map_stats_df = pd.concat([map_stats_df, pd.DataFrame.from_records([{
+            'Map': map_,
+            'Round Number': round_num,
+            'Team 1': team_names[0],
+            'Team 2': team_names[1],
+            'Team 1 Score': team_1_score,
+            'Team 2 Score': team_2_score,
+            'ATK Team': atk_team,
+            'DEF Team': def_team,
+            'Site': site,
+            'Winning Team': winning_team,
+            'Winning Side': winning_side,
+            'Won by Objective': won_by_objective
+        }])], ignore_index=True)
+
+    return map_stats_df
+
+
+def parse_json_player_stats(replay_json):
+    player_df = pd.DataFrame(columns=['player', 'team', 'map', 'kills', 'deaths', 'assists', 'headshots', 'objectives', 'trades', 'opening kill', 'opening death', '2ks', '3ks', '4ks', 'aces', 'rounds', 'kost rounds', 'suicides', 'teamkills', '1vX'])
+
+    # Stats from json stats section
+    for player in replay_json['stats']:
+        player_df = pd.concat([
+            player_df,
+            pd.DataFrame.from_records([{
+                'player': player['username'],
+                'rounds': player['rounds'],
+                'kills': player['kills'],
+                'deaths': player['deaths'],
+                'assists': player['assists'],
+                'headshots': player['headshots']
+            }])
+        ], ignore_index=True)
+
+    # Player's teams
+    players_teams = {}
+    for player in replay_json['rounds'][0]['players']:
+        username = player['username']
+        team_index = player['teamIndex']
+        team_name = replay_json['rounds'][0]['teams'][team_index]['name']
+        players_teams[username] = team_name
+    player_df = player_df.assign(team=player_df['player'].map(players_teams))
+
+    # Map
+    player_df['map'] = replay_json['rounds'][0]['map']['name']
+
+    # Objective
+    player_df['objectives'] = 0
+    objective_log = []
+    for round_num, round_ in enumerate(replay_json['rounds']):
+        if round_['matchFeedback'] is None:  # Skip if no matchFeedback
+            continue
+        for event in round_['matchFeedback']:
+            if event['type']['name'] == 'DefuserPlantComplete' or event['type']['name'] == 'DefuserDisableComplete' and (round_num, event['username']) not in objective_log:
+                player = event['username']
+                player_df.loc[player_df['player'] == player, 'objectives'] += 1
+                objective_log.append((round_num, player))
+
+    # Trades
+    player_df['trades'] = 0
+    kill_feed = []
+    for round_ in replay_json['rounds']:
+        round_kill_feed = []
+        if round_['matchFeedback'] is None:  # Skip if no matchFeedback
+            continue
+        for event in round_['matchFeedback']:
+            if event['type']['name'] == 'Kill':
+                killer = event['username']
+                killed = event['target']
+                time = event['timeInSeconds']
+                round_kill_feed.append((killer, killed, time))
+        kill_feed.append(round_kill_feed)
+
+    # Trade counts if someone kills someone who just got a kill within 10 seconds
+    trade_log = []
+    for round_num, round_kills in enumerate(kill_feed):
+        for i in range(len(round_kills)):
+            for j in range(i + 1, len(round_kills)):
+                if round_kills[i][0] == round_kills[j][1] and abs(round_kills[j][2] - round_kills[i][2]) <= 10:
+                    player_df.loc[player_df['player'] == round_kills[j][0], 'trades'] += 1
+                    trade_log.append((round_num, round_kills[j][0]))
+
+    # Opening kills
+    player_df['opening kill'] = 0
+    for round_kills in kill_feed:
+        if len(round_kills):
+            opening_kill = round_kills[0][0]
+            opening_death = round_kills[0][1]
+            player_df.loc[player_df['player'] == opening_kill, 'opening kill'] += 1
+            player_df.loc[player_df['player'] == opening_death, 'opening death'] += 1
+
+    # Opening death
+    player_df['opening death'] = 0
+    for round_ in replay_json['rounds']:
+        if round_['matchFeedback'] is None:
+            continue
+
+        for event in round_['matchFeedback']:
+            if event['type']['name'] == 'Kill':
+                player_df.loc[player_df['player'] == event['target'], 'opening death'] += 1
+                break
+            elif event['type']['name'] == 'Death':
+                player_df.loc[player_df['player'] == event['username'], 'opening death'] += 1
+                break
+
+    # 2k, 3k, 4k, ace
+    player_df['2ks'] = 0
+    player_df['3ks'] = 0
+    player_df['4ks'] = 0
+    player_df['aces'] = 0
+    for round_ in replay_json['rounds']:
+        for player in round_['stats']:
+            if player['kills'] == 2:
+                player_df.loc[player_df['player'] == player['username'], '2ks'] += 1
+            elif player['kills'] == 3:
+                player_df.loc[player_df['player'] == player['username'], '3ks'] += 1
+            elif player['kills'] == 4:
+                player_df.loc[player_df['player'] == player['username'], '4ks'] += 1
+            elif player['kills'] == 5:
+                player_df.loc[player_df['player'] == player['username'], 'aces'] += 1
+
+    # KOST rounds
+    player_df['kost rounds'] = 0
+    for round_num in range(len(replay_json['rounds'])):
+        for player in player_df['player'].values:
+            survived, got_kill, got_trade, did_objective = False, False, False, False
+            for player_stat in replay_json['rounds'][round_num]['stats']:
+                if player_stat['username'] == player:
+                    survived = not player_stat['died']
+                    got_kill = player_stat['kills'] > 0
+            for trade in trade_log:
+                if trade[0] == round_num and trade[1] == player:
+                    got_trade = True
+            if replay_json['rounds'][round_num]['matchFeedback'] is None:  # Skip if no matchFeedback
+                continue
+            for event in replay_json['rounds'][round_num]['matchFeedback']:
+                if event['username'] == player and event['type']['name'] == 'DefuserPlantComplete':
+                    did_objective = True
+            if survived or got_kill or got_trade or did_objective:
+                player_df.loc[player_df['player'] == player, 'kost rounds'] += 1
+
+    # Suicides
+    player_df['suicides'] = 0
+    for round_ in replay_json['rounds']:
+        if round_['matchFeedback'] is None:  # Skip if no matchFeedback
+            continue
+        for event in round_['matchFeedback']:
+            if event['type']['name'] == 'Death':
+                player_df.loc[player_df['player'] == event['username'], 'suicides'] += 1
+
+    # Teamkills
+    player_df['teamkills'] = 0
+    for round_num, round_feed in enumerate(kill_feed):
+        for kill in round_feed:
+            killer, target = kill[0], kill[1]
+
+            killer_team_index = 0
+            target_team_index = 0
+            for player in replay_json['rounds'][round_num]['players']:
+                if player['username'] == killer:
+                    killer_team_index = player['teamIndex']
+                elif player['username'] == target:
+                    target_team_index = player['teamIndex']
+
+            if killer_team_index == target_team_index:
+                player_df.loc[player_df['player'] == killer, 'teamkills'] += 1
+
+    # 1vX clutches
+    player_df['1vX'] = 0
+    for round_ in replay_json['rounds']:
+        team_names = player_df['team'].unique()
+        team_1_players = list(player_df[player_df['team'] == team_names[0]]['player'].values)
+        team_2_players = list(player_df[player_df['team'] == team_names[1]]['player'].values)
+
+        for player in round_['stats']:
+            if player['died']:
+                if player['username'] in team_1_players:
+                    team_1_players.remove(player['username'])
+                elif player['username'] in team_2_players:
+                    team_2_players.remove(player['username'])
+
+        # If only one left on your team and your team won the round
+        if len(team_1_players) == 1:
+            team_index = 0
+            for player in round_['players']:
+                if player['username'] == team_1_players[0]:
+                    team_index = player['teamIndex']
+
+            if round_['teams'][team_index]['won']:
+                player_df.loc[player_df['player'] == team_1_players[0], '1vX'] += 1
+        elif len(team_2_players) == 1:
+            team_index = 0
+            for player in round_['players']:
+                if player['username'] == team_2_players[0]:
+                    team_index = player['teamIndex']
+
+            if round_['teams'][team_index]['won']:
+                player_df.loc[player_df['player'] == team_2_players[0], '1vX'] += 1
+
+    return player_df
